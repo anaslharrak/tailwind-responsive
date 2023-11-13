40ffd588d5ba5d96e294a062c46640d58a14f71c
Anas Lharrak <146816560+anaslharrak@users.noreply.github.com>
2023-11-13 11:49:01 +0100

    finished testing with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import '../index.css'
+import exampleImage from './assets/example.jpg'
+
+function App() {
+
+  return (
+    <>
+      <section className='grid gap-8 text-center md:grid-cols-2 md:items-center md:text-left md:max-lg:bg-orange-400 lg:bg-blue-300 '>
+
+        <div className="">
+          <img src={exampleImage} alt="" className='w-full rounded-lg' />
+        </div>
+
+        <div>
+          <h1 className='mb-2 text-4xl font-medium'>Headline</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Voluptatibus cum quidem odio dicta ipsam nesciunt earum numquam ex nulla facere. 
+            Laborum est qui quasi animi cum alias tempore, sint ut. Lore</p>
+        </div>
+
+      </section>    
+    </>
+  )
+}
+
+export default App
