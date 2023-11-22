93bbbf9df9c699541fb03f322db3c17a8f7a6469
Anas Lharrak <146816560+anaslharrak@users.noreply.github.com>
2023-11-22 10:05:42 +0100

    dummy commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <section className='grid gap-8 text-center bg-green-400 md:grid-cols-2 md:items-center md:text-left md:max-lg:bg-orange-400 lg:bg-blue-300 '>
+      <section className='grid h-auto w-10/12 items-center p-10 gap-8 text-center rounded-lg grid-cols-1 bg-green-400 md:grid-cols-2 md:items-center md:text-left md:max-lg:bg-orange-400 lg:bg-blue-300 '>
 
         <div className="">
           <img src={exampleImage} alt="" className='w-full rounded-lg' />
